042ce094602684a8f157b003bd92cac1d60c1834
Dond <39187637+ItsJustDond@users.noreply.github.com>
2020-04-11 09:17:53 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,10 @@
 const Discord = require("discord.js");
 const config = require('../important.json')
 module.exports.run = async (client, message, args) => {
-    if(message.author.id !== config.ownerID) return;
 
 setInterval(function() {
-    message.channel.send("owo");
     message.channel.send(";sell");
 }, 10000);
-
-setInterval(function() {
-    message.channel.send("owoh");
-}, 15000);
 
 setInterval(function() {
     message.channel.send(";fish");
